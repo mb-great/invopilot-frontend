2c496ebe377f4023b6ef681032b00c209afd8bd9
Moksh <mokshbudhiraja@gmail.com>
2026-06-11 20:18:11 +0530

    chore: remove scratch files from frontend

--- test-isolation.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ path: './.env.local' });
-const { createClient } = require('@supabase/supabase-js');
-
-async function run() {
-  console.log("Not implemented yet");
-}
-run();
